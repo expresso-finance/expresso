bb2b0107e19ec8cd9ff3a1ef450bfc5b1935d7d8
Lorenzo S <103151696+expresso-finance@users.noreply.github.com>
2022-04-30 01:05:20 +0200

    Update ExsFeesManager.sol

--- ExsFeesManager.sol
+++ ExsFeesManager.sol
@@ -4,12 +4,17 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ExsFeesManager is Ownable {
-    mapping(uint=>uint) private _chainFees;
+   mapping(uint=>uint[]) private _chainFees;
+   mapping(uint32=>uint32) private _contractIndex; // contract Id (defined off-chain) -> index
 
-    function getChainFee(uint chainId) public view returns(uint){
-       return _chainFees[chainId];
-    }
-    function setChainFee(uint chainId, uint fee) external onlyOwner{
-       _chainFees[chainId]=fee;
-    }
+   function getChainFee(uint chainId, uint32 contractId) public view returns(uint){
+      return _chainFees[chainId][_contractIndex[contractId]];
+   }
+   function setChainFee(uint chainId, uint32 contractId, uint fee) external onlyOwner{
+      _chainFees[chainId][_contractIndex[contractId]]=fee;
+   }
+   function setContractIndex(uint32 contractId, uint32 index) external onlyOwner{
+      require(index>0,"Index must be greater then 0");
+      _contractIndex[contractId]=index;
+   }
 }
